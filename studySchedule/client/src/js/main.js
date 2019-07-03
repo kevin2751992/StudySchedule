77eab43455741c4a960d1f9610cfd2b215a51a27
Tim <lambrect@hochschule-trier.de>
2019-07-03 13:09:01 +0200

    edit content to index.html
    Abfrage ECTS  und speichern

--- studySchedule/client/src/js/main.js
+++ studySchedule/client/src/js/main.js
@@ -2,5 +2,30 @@ function hello() {
 	console.log("hello du spast");
 }
 
-window.hello = hello;
+let minEcts = "";
+let ectsPerSem = "";
+let numberOfSem = "";
 
+function speichern() {
+	//checkFormular() // auf number überprüfen, validieren der userdaten
+	minEcts = document.getElementById("minEcts").value;
+	ectsPerSem = document.getElementById("ectsPerSem").value;
+	numberOfSem = document.getElementById("numberOfSem").value;
+	console.log("ECTS:", minEcts);
+	console.log("ectsPerSem:", ectsPerSem);
+	console.log("numberOfSem:", numberOfSem);
+	if (minEcts === "") {
+		alert("Sie müssen alle Felder ausfuellen!");
+	}
+	else {
+		return;
+	}
+}
+
+function tabelproduce() {
+	// semesteranzahl angegeben= Zeilen
+	// wieviele ECTS pro semester = Spalten
+}
+
+window.hello = hello;
+window.speichern = speichern;
